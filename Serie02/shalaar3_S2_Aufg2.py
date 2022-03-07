d636ef9010a700579a517d133febd1a4170324b8
Armando Shala <shalaar3@students.zhaw.ch>
2022-03-07 21:39:15 +0100

    S2 A2 optimized

--- Serie02/shalaar3_S2_Aufg2.py
+++ Serie02/shalaar3_S2_Aufg2.py
@@ -6,33 +6,30 @@ x1, x2, x3 = sp.symbols('x1, x2, x3')
 def a1():
     f1 = 5 * x1 * x2
     f2 = x1 ** 2 * x2 ** 2 + x1 + 2 * x2
-
-    f = sp.Matrix([f1, f2])
-    x = sp.Matrix([x1, x2])
-
-    Df = f.jacobian(x)
-    print("jacobian:")
-    print(Df)
-
-    Df0 = Df.subs([(x1, 1), (x2, 2)])
-    print(Df0)
-    print(Df0.evalf())
+    jacobi([f1, f2], [x1, x2])
 
 
 def a2():
     f1 = sp.log(x1 ** 2 + x2 ** 2) + x3 ** 2
     f2 = sp.exp(x2 ** 2 + x2 ** 2) + x1 ** 2
     f3 = 1 / (x3 ** 2 + x1 ** 2) + x2 ** 2
+    jacobi([f1, f2, f3], [x1, x2, x3])
 
-    f = sp.Matrix([f1, f2, f3])
-    x = sp.Matrix([x1, x2, x3])
+
+def jacobi(functions, x_vals):
+    f = sp.Matrix(functions)
+    x = sp.Matrix(x_vals)
 
     Df = f.jacobian(x)
     print("jacobian:")
     print(Df)
-    Df0 = Df.subs([(x1, 1), (x2, 2), (x3, 3)])
-    print(Df0)
 
+    subList = []
+    for ind, x_val in enumerate(x_vals):
+        subList.append((x_val, ind + 1))
+
+    Df0 = Df.subs(subList)
+    print(Df0)
     print(Df0.evalf())
 
 
